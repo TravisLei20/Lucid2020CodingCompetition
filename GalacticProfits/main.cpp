0d0e2060227b8c848208c2873ea861404dddcacf
Travis <travisreynertson@gmail.com>
2021-11-05 01:35:50 -0600

    hi

--- GalacticProfits/main.cpp
+++ GalacticProfits/main.cpp
@@ -6,39 +6,96 @@
 using namespace std;
 
 int main() {
-    int months, stockNum;
+    int months, stock;
     cin >> months;
-    cin >> stockNum;
+    cin >> stock;
 
-    int myArray[stockNum][months];
+    int myArray[stock][months];
 
     string thing;
 
-    for (int i = 0; i < stockNum; ++i) {
+    for (int stockIndex = 0; stockIndex < stock; ++stockIndex)
+    {
         cin >> thing;
-        for (int j = 0; j < months; ++j) {
-            cin >> myArray[i][j];
-            cout << myArray[i][j] << endl;
+        for (int monthIndex = 0; monthIndex < months; ++monthIndex)
+        {
+            cin >> myArray[stockIndex][monthIndex];
         }
     }
 
-    vector<int> monthNum, whichStock, difference;
+    int difference[stock][months-1];
 
-    for (int i = 0; i < stockNum; ++i) {
-        for (int j = 0; j < months; ++j) {
-            if (myArray[i][j] > myArray[i][months-1])
+    for (int stockIndex = 0; stockIndex < stock; ++stockIndex)
+    {
+        for (int monthIndex = 0; monthIndex < months-1; ++monthIndex)
+        {
+            difference[stockIndex][monthIndex] = (myArray[stockIndex][months - 1] - myArray[stockIndex][monthIndex]);
+        }
+    }
+
+    int maxValues;
+    int minValues;
+
+    int stockShareMax[stock] ;
+    int stockShareMin[stock];
+
+    for (int stockIndex = 0; stockIndex < stock; ++stockIndex)
+    {
+        stockShareMax[stockIndex] = 0;
+        stockShareMin[stockIndex] = 0;
+    }
+
+    int maxSpent = 0;
+    int minSpent = 0;
+
+    for (int monthIndex = 0; monthIndex < months - 1 ; ++monthIndex)
+    {
+        maxValues = 0;
+        minValues = 0;
+        int whichStockMax = -1;
+        int whichStockMin = -1;
+        for (int stockIndex = 0; stockIndex < stock; ++stockIndex)
+        {
+            if (maxValues < difference[stockIndex][monthIndex])
             {
-                monthNum.push_back(j);
-                whichStock.push_back(i);
-                difference.push_back(myArray[i][j] - myArray[i][months-1]);
+                maxValues = difference[stockIndex][monthIndex];
+                whichStockMax = stockIndex;
+            }
+
+            if (minValues > difference[stockIndex][monthIndex])
+            {
+                minValues = difference[stockIndex][monthIndex];
+                whichStockMin = stockIndex;
             }
         }
-    }
-
-    for (int i = 0; i < whichStock.size(); ++i) {
-        for (int j = 0; j < monthNum.size(); ++j) {
-
+        if (whichStockMax != -1)
+        {
+            stockShareMax[whichStockMax] += (5000 / myArray[whichStockMax][monthIndex]) ;
+            maxSpent += 5000;
         }
+
+        if (whichStockMin != -1)
+        {
+            stockShareMin[whichStockMin] += (5000 / myArray[whichStockMin][monthIndex]);
+            minSpent += 5000;
+        }
+
     }
+
+    int totalMax = 0;
+    int totalMin = 0;
+
+    for (int stockIndex = 0; stockIndex < stock; ++stockIndex)
+    {
+        totalMax += stockShareMax[stockIndex] * myArray[stockIndex][months - 1];
+        totalMin += stockShareMin[stockIndex] * myArray[stockIndex][months - 1];
+    }
+
+    totalMax -= maxSpent;
+    totalMin -= minSpent;
+
+    cout << "Max: " << totalMax<< endl;
+    cout << "Min: " << totalMin;
+
     return 0;
 }
